a508a8ab17f9f722cea730f2540137a7402b7148
timurbaigashev <37540@iitu.edu.kz>
2025-10-30 16:58:27 +0500

    Fixed some localization errors

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -4,14 +4,16 @@ import ServicesItem from "../ServicesItem";
 import TitleBadge from "../TitleBadge";
 import styles from "./services.module.css";
 import { FaCompass } from "react-icons/fa";
-import { useServicesItems } from "@/content/services"; 
+import { useServicesItems } from "@/content/services";
+import { useTranslations } from "next-intl";
 
 const Services = () => {
-  const servicesItems = useServicesItems(); 
+  const servicesItems = useServicesItems();
+  const t = useTranslations("services");
 
   return (
     <div className={styles.container}>
-      <TitleBadge title="Основные направления" icon={FaCompass} />
+      <TitleBadge title={t("mainDirectionsTitle")} icon={FaCompass} />
       <div className={styles.items}>
         {servicesItems.map((item, index) => (
           <ServicesItem key={index} {...item} />
